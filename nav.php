aee8371a3d39d2c0b9335125b84a9bd204c28a87
Shidqi <92006908+Shidqi@users.noreply.github.com>
2021-11-15 11:02:23 +0700

    Update Navigation dan beberapa halaman

--- nav.php
+++ nav.php
@@ -1,12 +1,12 @@
 <ul>
-    <li class="active"><a href="home.php" title="Beranda">Beranda</a></li>
-        <li class="has-sub"><a href="layanan.php" title="">Layanan</a>
+    <li class="active"><a href="home.php" title="Beranda">Home</a></li>
+        
+    <li class="active"><a href="product.php" title="">Product</a></li>
+    <li class="has-sub"><a href="" title="">After Sales</a>
             <ul>
-                <li><a href="pemesanan.php?menu=pesan" title="Katakan Kebutuhan Anda Sekarang">Hubungi Kami</a></li>
+                <li><a href="pemesanan.php?menu=pesan" title="">Service</a></li>
             </ul>
         </li>
-        <li class="active"><a href="pengumuman.php" title="">Tentang Kami</a></li>
-    <li class="active"><a href="portofolio.php?menu=portofolio" title="">Portofolio</a></li>
     <li class="active"><a href="kontak.php" title="">Contact Us</a></li>
     <li class="active"><a href="karir.php?menu=karir" title="">Karir</a></li>
 </ul>
--- pemesanan.php
+++ pemesanan.php
@@ -56,7 +56,7 @@
             <div class='col-lg-8 col-md-8 col-sm-8 col-xs-12'>
         <div class='row'>
             <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
-                <h1>Sampaikan kebutuhan anda sekarang</h1>
+                <h1>Layanan Antrian Online Service</h1>
                     
                         <form method='post' action=''>
                                 <div class='row'>
