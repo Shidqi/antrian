2c40f18e1c3c3eca361a2c13dfe5471ebed554b5
Shidqi <92006908+Shidqi@users.noreply.github.com>
2021-11-23 11:46:01 +0700

    db back

--- dbbak/antrian.sql
+++ dbbak/antrian.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 18, 2021 at 09:38 AM
+-- Generation Time: Nov 23, 2021 at 05:45 AM
 -- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.12
+-- PHP Version: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,7 +42,29 @@ CREATE TABLE `tbl_antrian` (
 --
 
 INSERT INTO `tbl_antrian` (`id_antrian`, `nama`, `no_telfon`, `email`, `jenisservice`, `masalah`, `stnk`) VALUES
-(1, 'shidqi', 987654321, 'shidqi@mail.com', 'servicepenuh', 'bannya itu bang dia bermasalah susah di gerakin gitu olinya trus mesihnnya udah ganti semua aja kali ya', '');
+(1, 'shidqi', 987654321, 'shidqi@mail.com', 'servicepenuh', 'bannya itu bang dia bermasalah susah di gerakin gitu olinya trus mesihnnya udah ganti semua aja kali ya', ''),
+(2, '123123', 1231231, 'shidqimaulanasyifa99@gmail.com', 'servicepenuh', 'ganti oli', ''),
+(3, 'shodik', 1234567890, 'test@mail.com', 'servicepenuh', '-', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(11) NOT NULL,
+  `nama_user` varchar(50) NOT NULL,
+  `pass_user` varchar(50) NOT NULL,
+  `type_user` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama_user`, `pass_user`, `type_user`) VALUES
+(1, 'admin', 'admin', 'super admin');
 
 --
 -- Indexes for dumped tables
@@ -55,6 +77,12 @@ ALTER TABLE `tbl_antrian`
   ADD PRIMARY KEY (`id_antrian`);
 
 --
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,7 +90,13 @@ ALTER TABLE `tbl_antrian`
 -- AUTO_INCREMENT for table `tbl_antrian`
 --
 ALTER TABLE `tbl_antrian`
-  MODIFY `id_antrian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_antrian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
